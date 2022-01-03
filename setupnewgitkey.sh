7834a4d02fda15666250941078957fdf55627c0a
Kevin De Keyser <kevin@dekeyser.ch>
2022-01-03 16:48:13 +0000

    made sure user enters an email

--- setupnewgitkey.sh
+++ setupnewgitkey.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
-ssh-keygen -t ed25519 -C "dekeyser.kevin97@gmail.com"
+
+regex="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?\$"
+
+if [[ $1 =~ $regex ]] ; then
+    echo "Generating your ssh key with email ${1} now."
+else
+    echo "You must enter an email as an argument."
+    exit 1
+fi
+
+ssh-keygen -t ed25519 -C "$1"
 eval "$(ssh-agent -s)"
 mkdir -p .ssh && cp .ssh/config ~/.ssh/config
 
