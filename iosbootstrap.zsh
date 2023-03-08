912a6cd1d0104f71b7f2d3121f1be667352e741d
Kevin De Keyser <kevin@dekeyser.ch>
2023-03-08 01:44:03 +0100

    iosbootstrap

---
+++ iosbootstrap.zsh
@@ -0,0 +1,23 @@
+## manual way: install cydownload for installing cydia tools
+# https://github.com/borishonman/cydownload/releases
+# Download spotify decrypted IPA files:
+# open https://armconverter.com/decryptedappstore
+# Download spotilife using cydownload
+# https://julio.hackyouriphone.org/description.html?id=com.julioverne.spotilife
+# Apply patch using sideloadly
+# Note eg. http://apt.thebigboss.org/repofiles/cydia/
+
+# Youtube repo:
+# https://github.com/qnblackcat/uYouPlus/releases/
+# Spotify repo:
+# https://github.com/SpotCompiled/SpotC-Plus-Plus/releases
+# Instagram rocket:
+# https://github.com/qnblackcat/IGSideloadFix
+# Rhino Instagram:
+# https://lemamichael.github.io/WhatIsRhino/
+# TikTok:
+# https://github.com/ipahost/Unicorn-for-TikTok
+# TikTok god:
+# https://github.com/haoict/tiktok-god/releases
+
+# https://github.com/purp0s3/Tweaked-iOS-Apps/releases/
