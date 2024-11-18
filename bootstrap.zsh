12f074626e7cdfe1e236901505eb07b4412f4b61
Kevin De Keyser <kevin@dekeyser.ch>
2024-11-18 14:17:17 +0100

    small fixes

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -279,16 +279,21 @@ fi
 if [ $CRYPTO = 1 ]; then
   echo "Next: Installing monero."
   waitconfirm
-   git clone --recursive https://github.com/monero-project/monero ~/monero
+  if [ ! -d ~/monero ]; then
+    git clone --recursive https://github.com/monero-project/monero ~/monero
+  fi
   {
     cd ~/monero
+    chmod -R u+rw ~/monero
     git fetch --all
     BRANCH=$(git ls-remote --heads origin | grep 'release-' | awk -F'/' '{print $3}' | sort -V | tail -n 1)
     echo "Using monero branch: ${BRANCH}"
     git checkout --recurse-submodules ${BRANCH}
     git submodule init
     git submodule update
-    brew update && brew bundle --file=contrib/brew/Brewfile
+    brew update
+    brew bundle --file=contrib/brew/Brewfile || true # this command can fail due to empty brew taps.
+    chmod -R u+rw ~/monero
     make USE_SINGLE_BUILDDIR=0 # makes sure that only a single build dir is used.
     # a few notes
     # ledger is stored in $HOME/.bitmonero. Copy for faster sync.
