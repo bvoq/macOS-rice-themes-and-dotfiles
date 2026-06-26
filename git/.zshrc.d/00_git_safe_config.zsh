da3ba9dd86ca503d477975708e6cd1bea70182a3
Kevin De Keyser <dekeyser.kevin97@gmail.com>
2026-06-26 11:36:55 +0200

    Document git large file helper

--- git/.zshrc.d/00_git_safe_config.zsh
+++ git/.zshrc.d/00_git_safe_config.zsh
@@ -1,3 +1,4 @@
 alias gitroot="cd \$(git rev-parse --show-toplevel)"
+# find large git files in commit history.
 alias largegit="git rev-list --objects --all | git cat-file --batch-check='%(objecttype) %(objectname) %(objectsize) %(rest)' | awk '/^blob/ {print substr($""0,6)}' | sort --numeric-sort --key=2 | gnumfmt --field=2 --to=iec-i --suffix=B --padding=7 --round=nearest"
 
